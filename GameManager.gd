6f872d9c4f36c55d6e8ba05f7aba5d32c104d980
Charles Davenport <cdavenport090@gmail.com>
2021-10-19 23:56:20 -0400

    Create GameManager.gd

---
+++ GameManager.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+onready var player = get_node("Player")
+onready var asteroid_big = preload("res://Asteroid_Big.tscn")
+onready var asteroid_small = preload("res://Asteroid_Small.tscn")
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
