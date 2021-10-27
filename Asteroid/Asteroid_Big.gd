f5fed5dd3ed56eb9e2ef93e72bcd13fbb256dba7
Charles Davenport <cdavenport090@gmail.com>
2021-10-27 13:20:05 -0400

    remove bad line overriding speed var

--- Asteroid/Asteroid_Big.gd
+++ Asteroid/Asteroid_Big.gd
@@ -9,7 +9,6 @@ func destroy() -> void:
 		var asteroid_medium_inst = asteroid_medium.instance()
 		var rand_rot = rng.randf_range(0, TAU)
 		get_tree().root.add_child(asteroid_medium_inst)
-		asteroid_medium_inst.speed = 75 # probably should be defined in asteroid_small.gd
 		asteroid_medium_inst.start(position, rand_rot)
 	emit_signal("asteroid_destroyed", self)
 	queue_free()
