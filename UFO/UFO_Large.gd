113704f887ac96fde7746172897177a301e2e36c
Charles Davenport <cdavenport090@gmail.com>
2021-10-30 00:43:13 -0400

    ufo changes direction and has inaccuracy

--- UFO/UFO_Large.gd
+++ UFO/UFO_Large.gd
@@ -6,18 +6,33 @@ const projectile = preload("res://Projectile/Projectile.tscn")
 onready var screen_width = get_viewport_rect().size.x
 onready var screen_height = get_viewport_rect().size.y
 onready var shoot_timer = get_node("ShootTimer")
+onready var change_dir_timer = get_node("ChangeDirectionTimer")
+onready var rng = RandomNumberGenerator.new()
+onready var player = get_tree().root.get_node("Player")
 
-var target_point: Vector2
 var speed: float = 100.0
 var vel: Vector2
 
 signal ufo_destroyed
 
+func _ready():
+	rng.randomize()
+
 func start(a_point: Vector2, a_target: Vector2) -> void:
 	position = a_point
-	target_point = a_target
-	vel = (target_point - position).normalized()
+	vel = (a_target - position).normalized()
 	start_shooting()
+	start_changing_direction()
+
+func start_changing_direction() -> void:
+	while true:
+		change_dir_timer.start()
+		yield(change_dir_timer, "timeout")
+		change_direction()
+
+func change_direction() -> void:
+	var rand_rot = rng.randf_range(0, TAU)
+	vel = vel.rotated(rand_rot)
 
 func start_shooting() -> void:
 	shoot_timer.start()
@@ -28,14 +43,14 @@ func start_shooting() -> void:
 		shoot_at_player()
 
 func shoot_at_player() -> void:
-	print("UFO shooting at player")
-	var player = get_tree().root.get_node("Player")
 	if not player:
 		return
 	var dir_to_player = (player.position - position).normalized()
 	var projectile_inst = projectile.instance()
 	get_tree().root.add_child(projectile_inst)
-	projectile_inst.start(global_transform.origin, dir_to_player.angle(), projectile_inst.source_type.UFO)
+	# apply a random angle offset, so the ufo isn't perfectly accurate
+	var shoot_angle = dir_to_player.angle() + rng.randf_range(-TAU/10, TAU/10)
+	projectile_inst.start(global_transform.origin, shoot_angle, projectile_inst.source_type.UFO)
 	
 
 func _physics_process(delta):
@@ -49,7 +64,6 @@ func _physics_process(delta):
 	position.y = wrapf(position.y, 0, screen_height)
 
 func destroy() -> void:
-	print('destroying ufo')
 	emit_signal("ufo_destroyed")
 	queue_free()
 
