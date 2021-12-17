7561dbf247197173f3c9a67b6aa36fbecab7b6c2
Charles Davenport <cdavenport090@gmail.com>
2021-12-17 11:59:59 -0500

    scale crossover indicator sprite based on distance to edge of screen

--- Player/CrossOverIndicator.gd
+++ Player/CrossOverIndicator.gd
@@ -6,11 +6,13 @@ onready var screen_height = get_viewport_rect().size.y
 
 const vel_check_multiplier := 100.0
 const vel_check_max_dist := 400.0
+const default_scale := 0.05
 const padding := 30
 
 func update_indicator(player_pos: Vector2, player_vel: Vector2) -> void:
 	# detect if player is heading towards edge of the screen
 	self.visible = true
+	var intersect: Vector2
 	var new_pos: Vector2
 	var vel_check := (player_vel * vel_check_multiplier)
 	if vel_check.length() > vel_check_max_dist:
@@ -19,18 +21,26 @@ func update_indicator(player_pos: Vector2, player_vel: Vector2) -> void:
 	if vel_check.x < 0:
 		# find y-intercept of vel and screen edge
 		var y_int = player_pos.y - ((player_vel.y / player_vel.x) * player_pos.x)
-		new_pos = Vector2(screen_width - padding, y_int)
+		intersect = Vector2(0, y_int)
+		new_pos   = Vector2(screen_width - padding, y_int)
 	elif vel_check.x > screen_width:
 		var y_int = player_pos.y + ((player_vel.y / player_vel.x) * (screen_width - player_pos.x))
-		new_pos = Vector2(padding, y_int)
+		intersect = Vector2(screen_width, y_int)
+		new_pos   = Vector2(padding, y_int)
 	elif vel_check.y < 0:
 		var x_int = player_pos.x - ((player_vel.x / player_vel.y) * player_pos.y)
-		new_pos = Vector2(x_int, screen_height - padding)
+		intersect = Vector2(x_int, 0)
+		new_pos   = Vector2(x_int, screen_height - padding)
 	elif vel_check.y > screen_height:
 		var x_int = player_pos.x + ((player_vel.x / player_vel.y) * (screen_height - player_pos.y))
-		new_pos = Vector2(x_int, padding)
+		intersect = Vector2(x_int, screen_height)
+		new_pos   = Vector2(x_int, padding)
 	else:
 		self.visible = false
 		return
+	# use distance to edge of screen to scale the sprite
+	var dist = (intersect - player_pos).length()
+	var sprite_scale = default_scale * (vel_check_max_dist - dist) / vel_check_max_dist
+	self.scale = Vector2(sprite_scale, sprite_scale)
 	self.global_position = new_pos
 
